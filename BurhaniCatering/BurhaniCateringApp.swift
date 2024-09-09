b119067be8f5aab6286bedbbd394ab56ca8c2cc9
TahirSaifi-dxb <project.tahersaifi@gmail.com>
2024-09-10 00:24:35 +0400

    Initial Commit

---
+++ BurhaniCatering/BurhaniCateringApp.swift
@@ -0,0 +1,17 @@
+//
+//  BurhaniCateringApp.swift
+//  BurhaniCatering
+//
+//  Created by Tahir Saifi on 10/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct BurhaniCateringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BurhaniCateringTests/BurhaniCateringTests.swift
@@ -0,0 +1,16 @@
+//
+//  BurhaniCateringTests.swift
+//  BurhaniCateringTests
+//
+//  Created by Tahir Saifi on 10/09/2024.
+//
+
+import Testing
+
+struct BurhaniCateringTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
